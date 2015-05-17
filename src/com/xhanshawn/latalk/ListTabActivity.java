f1f1c1b8fc953188981e9424d786e719d2466c62
xhanshawn <xhan@wpi.edu>
2015-05-16 20:46:24 -0400

    v0.05b
    another commit for v0.05

---
+++ src/com/xhanshawn/latalk/ListTabActivity.java
@@ -0,0 +1,62 @@
+package com.xhanshawn.latalk;
+
+import java.util.ArrayList;
+
+import com.xhanshawn.data.LatalkMessage;
+import com.xhanshawn.util.MessageGetFactory;
+import com.xhanshawn.view.LatalkItemAdapter;
+import com.xhanshawn.view.MyListView;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ListTabActivity extends Activity {
+	
+	ArrayList<LatalkMessage> messages = new ArrayList<LatalkMessage>();
+	MyListView latalk_tv;
+	Context context;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_tab);
+		
+		latalk_tv = (MyListView) findViewById(R.id.list_tab_mlv);
+		
+		new MessageRetriever().execute("");
+		
+		context = this;
+        
+	}
+	
+	
+	public class MessageRetriever extends AsyncTask<String, Void, Boolean> {
+
+		@Override
+		protected Boolean doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			messages = MessageGetFactory.getPuzzleMessages();
+
+			return true;
+		}
+
+		@Override
+		protected void onPostExecute(Boolean result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			if(result){
+				LatalkItemAdapter latalk_item_adapter = new LatalkItemAdapter(context,messages);
+			
+				latalk_tv.setAdapter(latalk_item_adapter);
+			}
+		}
+		
+	}
+}
